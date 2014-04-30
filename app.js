9e99b15b3b718fecaf98d94326ee592ab1b35ce9
antoniocg507 <antoniocg507@gmail.com>
2014-04-29 21:46:19 -0500

    Se agregan la mayoria de los enrutamientos de las vista

--- app.js
+++ app.js
@@ -30,8 +30,30 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/admin',routes.admin);
+//Rutas para  Mision
 app.get('/admin/mision',routes.mision);
-app.get('/admin/npc/nuevo',routes.nuevoNPC);
+app.get('/admin/mision/nuevo',routes.misionNuevo);
+app.get('/admin/mision/modificar',routes.misionModificar);
+app.get('/admin/mision/borrar',routes.misionBorrar);
+app.get('/admin/mision/buscar',routes.misionBuscar);
+//Rutas para  NPC
+app.get('/admin/npc',routes.npc);
+app.get('/admin/npc/nuevo',routes.npcNuevo);
+app.get('/admin/npc/modificar',routes.npcModificar);
+app.get('/admin/npc/borrar',routes.npcBorrar);
+app.get('/admin/npc/buscar',routes.npcBuscar);
+//rutas para  Mundo
+app.get('/admin/mundo',routes.mundo);
+app.get('/admin/mundo/nuevo',routes.mundoNuevo);
+app.get('/admin/mundo/modificar',routes.mundoModificar);
+app.get('/admin/mundo/borrar',routes.mundoBorrar);
+app.get('/admin/mundo/buscar',routes.mundoBuscar);   
+//rutas para items
+app.get('/admin/item',routes.item);
+app.get('/admin/item/nuevo',routes.itemNuevo);
+app.get('/admin/item/modificar',routes.itemModificar);
+app.get('/admin/item/borrar',routes.itemBorrar);
+app.get('/admin/item/buscar',routes.itemBuscar);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,89 @@ exports.index = function(req, res){
 exports.admin = function(req, res){
     res.render('admin',{title : 'EduHeroes Admin' });
 }
-
+//rutas Mision
 exports.mision = function(req,res){
     res.render('mision',{title : 'EduHeroes Admin: Misiones'});
 }
 
-exports.nuevoNPC = function(req,res){
-    res.render('NPC/nuevoNpc',{});
+exports.misionNuevo = function(req,res){
+    res.render('mision/nuevo',{});
 }
+
+exports.misionModificar = function(req,res){
+    res.render('mision/modificar',{});
+}
+
+exports.misionBorrar = function(req,res){
+    res.render('mision/borrar',{});
+}
+
+exports.misionBuscar = function(req,res){
+    res.render('mision/buscar',{});
+}
+
+//Rutas NPC
+exports.npc = function(req,res){
+    res.render('npc',{});
+}
+
+exports.npcNuevo = function(req,res){
+    res.render('npc/nuevo',{});
+}
+
+exports.npcModificar = function(req,res){
+    res.render('npc/modificar',{});
+}
+
+exports.npcBorrar = function(req,res){
+    res.render('npc/borrar',{});
+}
+
+exports.npcBuscar = function(req,res){
+    res.render('npc/buscar',{});
+}
+
+//rutas Mundo
+
+exports.mundo = function(req,res){
+    res.render('mundo',{title : 'EduHeroes Admin: Misiones'});
+}
+
+exports.mundoNuevo = function(req,res){
+    res.render('mundo/nuevo',{});
+}
+
+exports.mundoModificar = function(req,res){
+    res.render('mundo/modificar',{});
+}
+
+exports.mundoBorrar = function(req,res){
+    res.render('mundo/borrar',{});
+}
+
+exports.mundoBuscar = function(req,res){
+    res.render('mundo/buscar',{});
+}
+
+//rutas Items 
+
+exports.item = function(req,res){
+    res.render('item',{});
+}
+
+exports.itemNuevo = function(req,res){
+    res.render('item/nuevo',{});
+}
+
+exports.itemModificar = function(req,res){
+    res.render('item/modificar',{});
+}
+
+exports.itemBorrar = function(req,res){
+    res.render('item/borrar',{});
+}
+
+exports.itemBuscar = function(req,res){
+    res.render('item/buscar',{});
+}
+
