bc13cd57b1c0b4cb46a94962d68b6df3064824c9
erika <erika.lizbeth994@gmail.com>
2014-05-28 12:27:18 -0500

    Creación de base de datos

--- baseEduHeroes.sql
+++ baseEduHeroes.sql
@@ -36,43 +36,47 @@ create table Logros(
 	fecha varchar(10),
 	tipoL int,
 	primary key(idLogro)
-);*/
+);
 
-	#Faltan por crear
-/*
 create table Npc(
 	idNpc int auto_increment not null,
 	vida int,
 	nombreN varchar(30),
 	daño_N int,
 	lvlN int,
-	idMundo int auto_increment not null,
+	idMundo int not null,
 	clase varchar(20),
-	idItem int auto_increment not null,
+	idItem int not null,
 	dialogo varchar(50),
 	primary key (idNpc),
 	foreign key (idItem) references Items(idItem),
 	foreign key (idMundo) references Mundo(idMundo)
 );
-/*
+create table Personaje(
+	idPj int auto_increment not null,
+	nombre varchar(15),
+	passwordP varchar(10),
+	primary key (idPj)
+);
+
 create table infoPersonaje(
 	idPj int auto_increment not null,
-	idMision int auto_increment not null,
+	idMision int not null,
 	posx int,
 	posy int,
 	lvlP int,
-	idMundo int auto_increment not null,
+	idMundo int not null,
 	puntos int,
 	primary key (idPj),
 	foreign key (idPj) references Personaje(idPj),
 	foreign key (idMision) references Misiones(idMision),
 	foreign key (idMundo) references Mundo(idMundo)
-);/*
+);
 create table inventarioPj(
 	idInventario int auto_increment not null,
-	idPj int auto_increment not null,
+	idPj int not null,
 	cantidadItems int,
-	idItem int auto_increment not null,
+	idItem int not null,
 	primary key (idInventario,idPj),
 	foreign key (idPj) references Personaje(idPj),
 	foreign key (idItem) references Items(idItem)
@@ -80,59 +84,27 @@ create table inventarioPj(
 
 create table misionesCompletadas(
 	idMision int auto_increment not null,
-	idPj int auto_increment not null,
+	idPj int not null,
 	primary key(idMision),
 	foreign key (idMision) references Misiones(idMision),
 	foreign key (idPj) references Personaje(idPj)
 );
-create table Personaje(
-	idPj int auto_increment not null,
-	idGrupo int auto_increment not null,
-	idTutor int auto_increment not null,
-	tipoPer varchar(20),
-	nombre varchar(15),
-	passwordP varchar(10),
-	primary key (idPj),
-	foreign key (idGrupo) references Grupos(idGrupo),
-	foreign key (idTutor) references Tutor(idTutor)
-);
-
 create table infoLogro(
 	idLogro int auto_increment not null,
-	idPj int auto_increment not null,
+	idPj int not null,
 	primary key (idLogro),
-	foreign key (idPj) references personaje(idPj)
+	foreign key (idPj) references Personaje(idPj)
 );
 create table datosPersona(
 	idPersona int auto_increment not null,
-	idPj int auto_increment not null,
+	idPj int not null,
 	nombre_p varchar(30),
 	direccion varchar(30),
 	ciudad varchar(20),
 	sexo varchar(10),
 	edad int,
 	email varchar(20),
+	tipoPer varchar(10),
 	primary key (idPersona),
-	foreign key (idPj) references personaje(idPj)
-);/*
-create table Jugador(
-	idPj int auto_increment not null,
-	primary key (idPersona)
-);
-create table Tutor(
-	idTutor int auto_increment not null,
-	primary key (idPersona)
-);
-create table Maestra(
-	idMaestra int auto_increment not null,
-	idGrupo int auto_increment not null,
-	primary key (idPersona),
-	foreign key (idGrupo) references Grupos(idGrupo)
-);
-create table Grupos(
-	idGrupo int auto_increment not null,
-	NombreG varchar(20),
-	idMaestra int auto_increment not null,
-	primary key (idGrupo),
-	foreign key (idMaestra) references maestra(idMastra)
-);*/
+	foreign key (idPj) references Personaje(idPj)
+);Hasta aqui estan creadas*/
